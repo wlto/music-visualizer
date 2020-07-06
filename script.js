97c2b071711f4357bcb7a4325b5f04f892caf62f
wlto <towilliam03@gmail.com>
2020-07-06 13:23:25 -0400

    Fix bugs

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-
 let song;
 let mediaFileType = `m4a`;
 let mediaFilePath = `assets/slchld-she-likes-spring-I-prefer-winter.m4a`;
@@ -11,10 +10,8 @@ let targetY = 0;
 class Attractor {
   constructor(x, y, m) {
     this.location = createVector(x, y);
-    this.velocity = createVector(0.1, 0.1);
-    this.acceleration = createVector(0, 0);
     this.mass = m;
-    this.G = 0.8;
+    this.G = 0.4;
   }
 
   // mover of type Mover
@@ -29,24 +26,6 @@ class Attractor {
     force.mult(strength);
 
     return force;
-  }
-
-  update() {
-    this.location.add(this.velocity);
-    this.velocity.add(this.acceleration);
-    
-    this.acceleration.mult(0);
-    this.velocity.limit(2);
-  }
-
-  updateLocation(location) {
-    this.location = createVector(location.x, location.y);
-  }
-
-  applyForce(force) {
-    var f = force.copy();
-    f.mult(1);
-    this.acceleration.add(f);
   }
 
   changeLocation(x, y) {
@@ -65,11 +44,11 @@ class Mover {
   }
 
   update() {
-    // if (this.history.length > 10) {
-    //   this.history.shift();
-    // }
+    if (this.history.length > 10) {
+      this.history.shift();
+    }
 
-    // this.history.push(createVector(this.location.x, this.location.y));
+    this.history.push(createVector(this.location.x, this.location.y));
 
     this.location.add(this.velocity);
     this.velocity.add(this.acceleration);
@@ -104,17 +83,6 @@ class Mover {
   }
 }
 
-var song;
-var fft;
-var attractor;
-var movers;
-var amplitude;
-var noseX;
-var noseY;
-
-var video;
-var poseNet;
-
 function preload() {
   soundFormats(mediaFileType);
   song = loadSound(mediaFilePath);
@@ -143,12 +111,11 @@ function setup() {
   video.hide();
 
   cvs.parent('canvasContainer');
-  background(255);
+  background(220);
   cvs.mousePressed(toggle);
 
   song.setVolume(0.5);
   song.play();
-  amplitude = new p5.Amplitude();
 
   fft = new p5.FFT(0.8, 256);
 
@@ -176,7 +143,7 @@ function draw() {
       energy = fft.getEnergy("bass");
     } else if (i <= 128) {
       energy = fft.getEnergy("mid");
-    } else if (i <= 256) {
+    } else if (i <= 192) {
       energy = fft.getEnergy("highMid");
     } else {
       energy = fft.getEnergy("treble");
@@ -187,15 +154,13 @@ function draw() {
     movers[i].update();
 
     noStroke();
-    fill(level*255, energy, spectrum[i], spectrum[i]);
+    fill(energy, 0, spectrum[i], 90);
     ellipse(
       movers[i].location.x, 
       movers[i].location.y, 
-      map(spectrum[i], 0, 255, 0, 30), 
-      map(spectrum[i], 0, 255, 0, 30)
+      map(spectrum[i], 0, 255, 0, 50), 
+      map(spectrum[i], 0, 255, 0, 50)
     );
-
-    fill(255, 0, 0, 100);
   }
 
 }
